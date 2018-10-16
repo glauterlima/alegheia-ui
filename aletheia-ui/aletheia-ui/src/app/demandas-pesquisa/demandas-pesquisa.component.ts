23ce491162652dbd5bb1799122ce4a5c56fb414d
Glauter Santos <glauterlima@gmail.com>
2018-10-16 15:10:57 -0300

    11.8 concluido - Desafio usando pipes

--- aletheia-ui/aletheia-ui/src/app/demandas-pesquisa/demandas-pesquisa.component.ts
+++ aletheia-ui/aletheia-ui/src/app/demandas-pesquisa/demandas-pesquisa.component.ts
@@ -9,10 +9,10 @@ export class DemandasPesquisaComponent {
 
   demandas = [
     { nome: 'SARH-78', sistema: 'SARH', pessoa: 'Glauter Santos',
-    lote: 'JANEIRO/2018', plataforma: 'ORACLEFORMS', totalPfPlataforma: 58.63, data: '25/09/2018'},
+    lote: 'JANEIRO/2018', plataforma: 'ORACLEFORMS', totalPfPlataforma: 58.63, data: new Date(2018, 10, 11), valor: 585},
     { nome: 'EAVS-89', sistema: 'EAVS', pessoa: 'Fernanda Paranaguá',
-    lote: 'FEVEREIRO/2018', plataforma: 'JAVA', totalPfPlataforma: 78.96, data: '25/10/2018'},
+    lote: 'FEVEREIRO/2018', plataforma: 'JAVA', totalPfPlataforma: 78.96, data: new Date(2018, 10, 12), valor: 874},
   { nome: 'SIREC-11', sistema: 'SARH', pessoa: 'Hanna Krycia',
-    lote : 'MARCO/2018', plataforma: 'ORACLEFORMS', totalPfPlataforma: 54.88, data: '25/11/2018'}
+    lote : 'MARCO/2018', plataforma: 'ORACLEFORMS', totalPfPlataforma: 54.88, data: new Date(2018, 10, 13), valor: 900}
   ];
 }
