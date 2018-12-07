6ca6b07e7ae40c775d00cfbdcb5e914902ffa1d1
Glauter Santos <glauterlima@gmail.com>
2018-12-07 21:28:26 -0200

    Commit sexta dia 07/12 saidera

--- aletheia-ui/aletheia-ui/src/app/app.module.ts
+++ aletheia-ui/aletheia-ui/src/app/app.module.ts
@@ -1,44 +1,41 @@
-import { Http, HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { ToastyModule } from 'ng2-toasty';
-import { ConfirmDialogModule} from 'primeng/components/confirmdialog/confirmdialog';
+import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
 import { ConfirmationService } from 'primeng/components/common/api';
 
 import { AppComponent } from './app.component';
-import { LinhaComponent } from './navegacao/linha/linha.component';
-import { DemandasModule } from './demandas/demandas.module';
 import { CoreModule } from './core/core.module';
-import { PessoaService } from './pessoas/pessoa.service';
 import { PessoasModule } from './pessoas/pessoas.module';
+import { DemandasModule } from './demandas/demandas.module';
+import { PessoaService } from './pessoas/pessoa.service';
 import { DemandaService } from './demandas/demanda.service';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LinhaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    CoreModule,
     HttpModule,
 
     ToastyModule.forRoot(),
     ConfirmDialogModule,
 
+    CoreModule,
     DemandasModule,
     PessoasModule
-
   ],
   providers: [
     DemandaService,
     PessoaService,
-    ConfirmationService
+    ConfirmationService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [AppComponent]
 })
